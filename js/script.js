05a7c23946cd6635122c9feb659d6f0e9ff61946
Mykola Kizan <mkizan@gmail.com>
2020-11-14 17:26:42 +0200

    added timer on js

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const newYears = "1 Jan 2021";
+
+const daysEl = document.querySelector('.days');
+const hoursEl = document.querySelector('.hours');
+const minutesEl = document.querySelector('.minutes');
+const secondsEl = document.querySelector('.seconds');
+// const secondsStr = document.querySelector('.unit-seconds')
+
+function countdown() {
+
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+ 
+    const totalSeconds = Math.floor((newYearsDate - currentDate) / 1000);
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = totalSeconds % 60;
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+
+    // secondsStr.innerHTML = endTime(secondsStr);
+
+}
+
+countdown();
+
+setInterval(countdown, 1000);
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// function endTime(num) {
+//     return num.toString()[1];
+//     // if(time[1] = (2 || 3 || 4)) {
+//     //     return `${secondStr}и`;
+//     // }
+// }
+
+// endTime(23);
