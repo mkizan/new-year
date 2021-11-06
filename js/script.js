08ddeb9d7306983407c1fe235fe08c9c924fee9c
mkizan <73556376+mkizan@users.noreply.github.com>
2021-11-06 10:18:13 +0200

    fixed

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
-const newYears = "1 Jan 2022";
+const newYears = "01 Jan 2022";
 
-const daysEl = document.querySelector('.days');
-const hoursEl = document.querySelector('.hours');
-const minutesEl = document.querySelector('.minutes');
-const secondsEl = document.querySelector('.seconds');
+const daysEl = document.querySelector(".days");
+const hoursEl = document.querySelector(".hours");
+const minutesEl = document.querySelector(".minutes");
+const secondsEl = document.querySelector(".seconds");
+const secondsStr = document.querySelector(".unit-seconds");
 
 function countdown() {
+  const newYearsDate = new Date(newYears);
+  const currentDate = Date.now();
 
-    const newYearsDate = new Date(newYears);
-    const currentDate = Date.now();
- 
-    const totalSeconds = Math.floor((newYearsDate - currentDate) / 1000);
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const minutes = Math.floor(totalSeconds / 60) % 60;
-    const seconds = totalSeconds % 60;
+  const totalSeconds = Math.floor((newYearsDate - currentDate) / 1000);
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = totalSeconds % 60;
 
-    daysEl.innerHTML = formatTime(days);
-    hoursEl.innerHTML = formatTime(hours);
-    minutesEl.innerHTML = formatTime(minutes);
-    secondsEl.innerHTML = formatTime(seconds);
-
+  daysEl.innerHTML = formatTime(days);
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
 }
 
 countdown();
